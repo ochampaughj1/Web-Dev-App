3b140dfacfb109327170185a25d0c61c1eb2a92d
ochampaughj1 <ochampaughj1@ksu.edu>
2024-04-04 15:30:57 -0500

    testing commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { SeriesPageComponent } from './series-page/series-page.component';
+import { BookPageComponent } from './book-page/book-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'series-page', component: SeriesPageComponent },
+    { path: 'book-page', component: BookPageComponent },
+  ];
