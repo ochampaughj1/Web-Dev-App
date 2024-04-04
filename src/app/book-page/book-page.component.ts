99055f0b7577ab08c8f0629b58b70975fcd79b36
ochampaughj1 <ochampaughj1@ksu.edu>
2024-04-04 15:35:18 -0500

    uploading files

---
+++ src/app/book-page/book-page.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-book-page',
+  standalone: true,
+  imports: [],
+  templateUrl: './book-page.component.html',
+  styleUrl: './book-page.component.css'
+})
+export class BookPageComponent {
+
+}
---
+++ src/app/series-page/series-page.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-series-page',
+  standalone: true,
+  imports: [],
+  templateUrl: './series-page.component.html',
+  styleUrl: './series-page.component.css'
+})
+export class SeriesPageComponent {
+
+}
