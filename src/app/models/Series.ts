c9184057be8022ca1cf576a5b95931553483bd00
ochampaughj1 <ochampaughj1@ksu.edu>
2024-05-09 11:34:30 -0500

    added series summaries

--- src/app/models/Series.ts
+++ src/app/models/Series.ts
@@ -3,10 +3,12 @@ export class Series {
     SeriesId: number;
     Title: string;
     NumBooks: number;
+    Description: string;
 
-    constructor(seriesId: number, title: string, numBooks: number) {
+    constructor(seriesId: number, title: string, numBooks: number, description: string) {
         this.SeriesId = seriesId;
         this.Title = title;
         this.NumBooks = numBooks;
+        this.Description = description;
     }
 }
